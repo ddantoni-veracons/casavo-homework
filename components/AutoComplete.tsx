75048c2034ad60ae3f07ef2fc3daa5553c015e15
ddantoni_veracons <ceo@veracons.com>
2024-12-04 09:13:51 +0100

    Added API call to fetch cities when the component mounts

--- components/AutoComplete.tsx
+++ components/AutoComplete.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { City } from '@/module/city.interface';
+import React, { useEffect, useState } from 'react';
 
 interface AutoCompleteProps {
   onSelectCity: (cityUUID: string) => void;
@@ -7,6 +8,23 @@ interface AutoCompleteProps {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const AutoComplete: React.FC<AutoCompleteProps> = ({ onSelectCity }) => {
   const [searchTerm, setSearchTerm] = useState('');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [cities, setCities] = useState<City[]>([]);
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        const response = await fetch(
+          'https://etherqmshqkpehcowxqh.supabase.co/functions/v1/cities'
+        );
+        const data: City[] = await response.json();
+        setCities(data);
+      } catch (error) {
+        console.error('Error fetching cities:', error);
+      }
+    };
+    fetchCities();
+  }, []);
 
   return (
     <div style={{ position: 'relative', width: '300px' }}>
---
+++ module/city.interface.ts
@@ -0,0 +1,5 @@
+export interface City {
+    uuid: string;
+    name: string;
+    country: string;
+}
